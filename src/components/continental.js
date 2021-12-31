8507169cd0557549b68ee8325cce7878dc2f9c59
unknown <sushantsb026@gmail.com>
2021-12-31 10:33:15 +0530

    homepage-cards-sushant

---
+++ src/components/continental.js
@@ -0,0 +1,35 @@
+import image from '../img/food.jpg'
+
+export const continental=[
+    {
+        img:image,
+        variety:"continental",
+        desc:"Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae",
+        price:" $ 100",
+    },{
+        img:image,
+        variety:"continental",
+        desc:"Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae",
+        price:" $ 100",
+    },{
+        img:image,
+        variety:"continental",
+        desc:"Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae",
+        price:" $ 100",
+    },{
+        img:image,
+        variety:"continental",
+        desc:"Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae",
+        price:" $ 100",
+    },{
+        img:image,
+        variety:"continental",
+        desc:"Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae",
+        price:" $ 100",
+    },{
+        img:image,
+        variety:"continental",
+        desc:"Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae",
+        price:" $ 100",
+    }
+]
